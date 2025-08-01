f4cc785cae3ce0bac1018335a3cb6de1883db46c
Jerhyme Maraginot <jmaraginot6161@liceo.edu.ph>
2025-08-01 11:25:11 +0800

    lab-activity-2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main Branch!");
+        Scanner input = new Scanner(System.in);
+        int total = 0;
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Enter number " + i + ": ");
+            int number = input.nextInt();
+            total += number;
+        }
+
+        System.out.println("Total Number: " + total);
+        input.close();
     }
 }
